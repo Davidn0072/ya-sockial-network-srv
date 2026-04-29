1d7e060cc67f3877c2e84ef5ad33971d24dd10d4
Davidn0072 <Davidn0072@Gmail.COM>
2026-04-29 12:39:41 +0300

    Refactor post model and repository for improved data handling
    - Removed the required title field from postSchema in postModel to allow for more flexible post creation.

--- models/postModel.js
+++ models/postModel.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema(
     {
-        title: { type: String, required: true, minlength: 3, maxlength: 100 },
         content: { type: String, required: true, minlength: 3, maxlength: 1000 },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now },
