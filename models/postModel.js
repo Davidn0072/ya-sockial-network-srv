e64524dbc931d40fca87113cee6e0ceeeb21b997
Davidn0072 <Davidn0072@Gmail.COM>
2026-05-02 11:24:34 +0300

    Enhance postSchema with additional index for improved query performance
    - Removed default timestamps for createdAt and updatedAt fields.
    - Added a new index on createdAt to optimize query efficiency.

--- models/postModel.js
+++ models/postModel.js
@@ -3,14 +3,13 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema(
     {
         content: { type: String, required: true, minlength: 3, maxlength: 1000 },
-        createdAt: { type: Date, default: Date.now },
-        updatedAt: { type: Date, default: Date.now },
         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     },
     { versionKey: false, timestamps: true }
 );
 
 postSchema.index({ userId: 1, createdAt: -1 });
+postSchema.index({ createdAt: -1 });
 const Post = mongoose.model('Post', postSchema, 'posts');
 
 export default Post;
